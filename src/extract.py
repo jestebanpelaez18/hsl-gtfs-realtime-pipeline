57c1d4ba74c3dc5097fac36c4d102811c0ac6a84
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2025-05-07 22:36:00 +0300

    feat(dag): register GTFS realtime extractor DAG with dynamic feeds

--- src/extract.py
+++ src/extract.py
@@ -1,9 +1,10 @@
 import requests
 import os
-import json 
+import json
 from google.transit import gtfs_realtime_pb2
 from google.protobuf.json_format import MessageToDict
 from datetime import datetime
+
 
 def fetch_gtfs_feed(url):
     ''' Fetches GTFS Realtime feed from HSL and parses it into a FeedMessage '''
@@ -16,9 +17,11 @@ def fetch_gtfs_feed(url):
         print("Error fetching URL", {e})
         return None
 
+
 def parse_feed(feed):
     ''' Parses a GTFS FeedMessage into a list of dicts using MessageToDict '''
     return [MessageToDict(entity, preserving_proto_field_name=True) for entity in feed.entity]
+
 
 def save_feed(data, feed_type, output_dir):
     ''' Saves parsed GTFS data to a timestamped JSON file '''
@@ -31,23 +34,27 @@ def save_feed(data, feed_type, output_dir):
 
     print(f"✅ Saved {len(data)} records to {file_path}")
 
+
 def extract_and_save(url, feed_type, output_dir):
     feed = fetch_gtfs_feed(url)
     data = parse_feed(feed)
     save_feed(data, feed_type, output_dir)
 
-# Run the extractor
-output_dir = "../data/raw"
 
-FEED_URL = {
-    "vehicle_position": 'https://realtime.hsl.fi/realtime/vehicle-positions/v2/hsl',
-    "trip_updates": 'https://realtime.hsl.fi/realtime/trip-updates/v2/hsl'
-}
+def run_all_feed(output_dir):
+    FEED_URL = {
+        "vehicle_position": 'https://realtime.hsl.fi/realtime/vehicle-positions/v2/hsl',
+        "trip_updates": 'https://realtime.hsl.fi/realtime/trip-updates/v2/hsl'
+    }
+    for feed_type, url in FEED_URL.items():
+        print(f"extracting feed: {feed_type}")
+        feed = fetch_gtfs_feed(url)
+        if feed:
+            data = parse_feed(feed)
+            save_feed(data, feed_type, output_dir)
 
-for feed_type, url in FEED_URL.items():
-    print(f"extracting feed: {feed_type}")
-    if feed := fetch_gtfs_feed(url):
-        data = parse_feed(feed)
-        save_feed(data, feed_type, output_dir)
 
-
+# Only run when i execute the script
+if __name__ == "__main__":
+    output_dir = "../data/raw"
+    run_all_feed(output_dir)
