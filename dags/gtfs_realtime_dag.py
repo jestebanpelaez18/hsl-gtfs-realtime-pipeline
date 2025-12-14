b6d0d7cd91f4f2795774672961c280d3efca5ef1
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2025-12-14 22:31:59 +0200

    feat(DAG): Added Airflow DAG that upload the vehicle position to Databricks Workspace

--- dags/gtfs_realtime_dag.py
+++ dags/gtfs_realtime_dag.py
@@ -5,14 +5,16 @@ from datetime import timedelta
 import sys
 sys.path.append('/opt/airflow/src')
 from extract import run_all_feed
+import os
+from databricks_utils import upload_to_databricks, upload_to_workspace
 
 
 default_args = {
     'owner': 'Juan Esteban',
     'start_date': days_ago(1),
     'email': 'juanillo@hello.com',
-    'email_on_failure': True,
-    'email_on_retry': True,
+    'email_on_failure': False,
+    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
@@ -29,6 +31,23 @@ def run_extractor():
     output_dir = "/opt/airflow/data/raw"
     run_all_feed(output_dir)
 
+def upload_latest_vehicle_positions():
+    raw_data_dir = "/opt/airflow/data/raw"
+
+    # Find the latest vehicle positions file
+    vehicle_files = [f for f in os.listdir(raw_data_dir) if f.startswith("vehicle_position_") and f.endswith(".json")]
+    if not vehicle_files:
+        raise FileNotFoundError("No vehicle position files found.")
+
+    latest_file = max(vehicle_files, key=lambda x: os.path.getctime(os.path.join(raw_data_dir, x)))
+    latest_file_path = os.path.join(raw_data_dir, latest_file)
+    
+    # Upload the latest vehicle positions file to Databricks
+    print(f"Uploading latest vehicle positions to Databricks: {latest_file_path}")
+    workspace_path = upload_to_workspace(latest_file_path)
+    print("Uploaded to workspace:", workspace_path)
+
+    return workspace_path
 
 
 # Define tasks
@@ -40,4 +59,12 @@ extract_data = PythonOperator(
     dag=dag
 )
 
-extract_data
+#Task 2: Upload Data to DataBricks
+upload_data_to_databricks = PythonOperator(
+    task_id='upload_data_to_databricks',
+    python_callable=upload_latest_vehicle_positions,
+    dag=dag
+)   
+
+
+extract_data >> upload_data_to_databricks
